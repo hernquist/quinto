43b7d9b050af83ffc1def79691f72956fedd647f
David Hernquist <hernquistdavid@gmail.com>
2025-04-13 16:31:49 -0400

    feat: add square component that may or may not show tile

---
+++ src/lib/components/game/Square.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import type { ISquare } from "./types";
+    import { dropzone } from "./dnd";
+
+    export let square: ISquare;
+    export let onDropzone
+
+</script>
+
+{#if square?.tile}
+    <div 
+        class="board__square" 
+        id={String(square.id)} 
+    >
+        {square.tile?.text}
+    </div>
+{:else}
+    <div 
+        class="board__square" 
+        id={String(square.id)} 
+        use:dropzone={{
+            on_dropzone: onDropzone
+        }}
+    ></div>
+{/if}
+
+
+<style>
+    .board__square {
+        width: 60px;
+        height: 60px;
+        background-color: lightsalmon;
+        border: 2px solid gray;
+        border-radius: 1px;
+    }
+</style>
