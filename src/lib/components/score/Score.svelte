c1eed26e6fea3d978cdc503825635e908c3dd481
David Hernquist <hernquistdavid@gmail.com>
2025-04-15 00:09:25 -0400

    feat: move Score.svelte and add scores

---
+++ src/lib/components/score/Score.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { gameState } from "../../../state/state.svelte";
+	import { Players } from "../../../state/types";
+</script>
+<div>{gameState[Players.Top].score} scoreboard {gameState[Players.Bottom].score}</div>
