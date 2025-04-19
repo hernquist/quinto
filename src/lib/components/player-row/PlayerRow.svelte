27b4f2d6f70f1b74fa2d69338c35d8f852349bf9
David Hernquist <hernquistdavid@gmail.com>
2025-04-19 14:22:05 -0400

    clean up player row

--- src/lib/components/player-row/PlayerRow.svelte
+++ src/lib/components/player-row/PlayerRow.svelte
@@ -1,21 +1,16 @@
 <script lang="ts">
-	import { getGameState } from "../../../state/state.svelte";
+	import { getGameState } from "../../state/state.svelte";
 	import FinishTurn from "../finish-turn/FinishTurn.svelte";
     import PlayerTiles from "../player-tiles/PlayerTiles.svelte";
-    import type { Players } from "../../../state/types";
 
-    const props = $props();
-    const playerPosition: Players = props.playerPosition
-
+    const { tiles, playerPosition } = $props();
     const gameState = getGameState();
     const { activePlayer } = gameState;
     const isActive = activePlayer === playerPosition;
-    const { tiles } = gameState[playerPosition];
-
 </script>
 
 <div>
-    <PlayerTiles {isActive} {tiles}/>
+    <PlayerTiles {tiles} {isActive}/>
     <FinishTurn {isActive} />
 </div>
 
