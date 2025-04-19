638899b7746a3f0b7906883a26a456d8c2532a7e
David Hernquist <hernquistdavid@gmail.com>
2025-04-19 14:19:15 -0400

    replace slot with @render

--- src/lib/components/initialize-game/InitializeGame.svelte
+++ src/lib/components/initialize-game/InitializeGame.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-    import { updateBoardDimensions } from "../../../state/state.svelte";
+    import { updateBoardDimensions } from "../../state/state.svelte";
 	import generateGameTiles from "./utils/generateGameTiles";
 	import initializePlayers from "./utils/initializePlayers";
 	import initializeBoard from "./utils/initializeBoard";
 
+    import { getPlayerTilesState } from "$lib/state/player.svelte";
+	import { Players } from "$lib/state/types";
+
     const ROWS = 7;
     const COLUMNS = 6;
+
+    const  { children } = $props();
 
     // initialize
     updateBoardDimensions(ROWS, COLUMNS);
     generateGameTiles();
-    initializePlayers();
+    const { topPlayerTiles } = initializePlayers();
+
+    const playerTileState = getPlayerTilesState()
+    
+    // updatePlayerTiles(Players.Top, topPlayerTiles);
+    playerTileState.update(topPlayerTiles)
+    
     initializeBoard();
 </script>
 
-<slot />
+{@render children()}
