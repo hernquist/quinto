6cce41d828ef5730823b8989ba542e953e91819a
David Hernquist <hernquistdavid@gmail.com>
2025-04-19 14:18:50 -0400

    working -- in progress

--- src/lib/components/game/Game.svelte
+++ src/lib/components/game/Game.svelte
@@ -3,13 +3,19 @@
     import Score from "../score/Score.svelte";
 	import InitializeGame from "../initialize-game/InitializeGame.svelte";
 	import PlayerRow from "../player-row/PlayerRow.svelte";
-    import { Players } from "../../../state/types";
+    import { Players } from "../../state/types";
+	import { getPlayerTilesState } from "$lib/state/player.svelte";
+    
     const { Top, Bottom } = Players;
+
+    const playerTileState = getPlayerTilesState();
+
 </script>
 
 <InitializeGame>
+    {playerTileState.tiles.length}
     <Score />
-    <PlayerRow playerPosition={Top}/>
+    <PlayerRow playerPosition={Top} tiles={playerTileState.tiles}/>
     <Board />
-    <PlayerRow playerPosition={Bottom}/>
+    <!-- <PlayerRow playerPosition={Bottom}/> -->
 </InitializeGame>
