22005841e5504d8ae35ba51e8425958da646ac1f
David Hernquist <hernquistdavid@gmail.com>
2025-04-19 14:17:45 -0400

    clean up on board.svelte

--- src/lib/components/board/Board.svelte
+++ src/lib/components/board/Board.svelte
@@ -1,25 +1,17 @@
 <script lang="ts">
-	import { getGameState, updateBoardSquare, updatePlayerTiles } from "../../../state/state.svelte";
+	import { getGameState } from "../../state/state.svelte";
 	import Square from "../square/Square.svelte";
-	import type { ITile } from "../game/types";
     
     const gameState = getGameState();
-    const { activePlayer, board } = gameState;
+    const { board } = gameState;
+
 </script>
 
 <div class="board__container">
     {#each board as column, x}
         <div class="board_row">
             {#each column as square, y}
-                {@const onDropzone = (tileId: number): void => {
-                    const foundTile: ITile | undefined = gameState[activePlayer].tiles.find(tile => tile.id == tileId);
-                    if (foundTile) updateBoardSquare(x, y, foundTile);
-                    const newTiles = gameState[activePlayer].tiles.filter(tile => {
-                        return tile.id != tileId
-                    });
-                    updatePlayerTiles(activePlayer, newTiles);
-                }}
-                <Square {square} {onDropzone}/>
+                <Square {square} {x} {y}/>
             {/each}
         </div>
     {/each}
