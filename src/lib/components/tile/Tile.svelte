43a996a31ca0e6e5de02ed2a20c1d264f933d15f
David Hernquist <hernquistdavid@gmail.com>
2025-04-22 22:27:04 -0400

    update css and font-family

--- src/lib/components/tile/Tile.svelte
+++ src/lib/components/tile/Tile.svelte
@@ -32,13 +32,16 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        margin: 2px;
+        margin: 2px; 
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        font-size: 42px;
     }
 
     .tile.disabled {
         background-color: dimgrey;
         color: linen;
         opacity: 1;
+        font-size: 40px;
     }
 </style>
 
