2c040125e90bb46c53c32736b33e5bfd8867bf85
David Hernquist <hernquistdavid@gmail.com>
2025-04-27 00:24:51 -0400

    update css

--- src/lib/components/toasts/Toaster.svelte
+++ src/lib/components/toasts/Toaster.svelte
@@ -6,41 +6,22 @@
 	const toastState: IToastState = getToastState();
 </script>
 
-<div class="absolute right-2 top-2 flex flex-col gap-2">
+<div class="toaster">
 	{#each toastState.toasts as toast}
 		<Toast {toast} />
 	{/each}
 </div>
 
-<!-- <script lang="ts">
-	import { dismissToast, toasts } from "$lib/state/toast";
-	import Toast from "./Toast.svelte";
-  
-  </script>
-  
-  {#if $toasts}
-    <section>
-      {#each $toasts as toast (toast.id)}
-        <Toast
-          type={toast.type}
-          dismissible={toast.dismissible}
-          on:dismiss={() => dismissToast(toast.id)}>{toast.message}</Toast
-        >
-      {/each}
-    </section>
-  {/if}
-  
-  <style lang="postcss">
-    section {
-      position: fixed;
-      top: 0;
-      left: 0;
-      right: 0;
-      width: 100%;
-      display: flex;
-      margin-top: 1rem;
-      justify-content: center;
-      flex-direction: column;
-      z-index: 1000;
-    }
-  </style> -->
+<style lang="postcss">
+  .toaster {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    margin-top: 1rem;
+    justify-content: center;
+    flex-direction: column;
+    z-index: 1000;
+  }
+</style>
