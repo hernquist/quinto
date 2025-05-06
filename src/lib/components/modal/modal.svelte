23bc260641fcfa2b23ffe24643cd720810e3c728
David Hernquist <hernquistdavid@gmail.com>
2025-05-06 14:25:12 -0400

    modal base component

--- src/lib/components/modal/modal.svelte
+++ src/lib/components/modal/modal.svelte
@@ -1,28 +1,32 @@
 <script>
-	let { showModal = $bindable(), header, children } = $props();
+	import { getModalState } from "$lib/state/modal-state/modal-state.svelte";
+
+	let { header, children } = $props();
+
+	const modalState = getModalState();
 
 	let dialog = $state(); // HTMLDialogElement
 
 	$effect(() => {
-		if (showModal) dialog.showModal();
+		if (modalState.showModal) dialog.showModal();
 	});
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_noninteractive_element_interactions -->
-<dialog
-	bind:this={dialog}
-	onclose={() => (showModal = false)}
-	onclick={(e) => { if (e.target === dialog) dialog.close(); }}
->
-	<div>
-		{@render header?.()}
-		<hr />
-		{@render children?.()}
-		<hr />
-		<!-- svelte-ignore a11y_autofocus -->
-		<button autofocus onclick={() => dialog.close()}>close modal</button>
-	</div>
-</dialog>
+	<dialog 
+		bind:this={dialog} 
+		onclose={modalState.toggleModalOff}
+		onclick={(e) => { if (e.target === dialog) dialog.close(); } }
+	>
+		<div>
+			{@render header?.()}
+			<hr />
+			{@render children?.()}
+			<hr />
+			<!-- svelte-ignore a11y_autofocus -->
+			<button autofocus onclick={() => modalState.toggleModalOff()}>close modal</button>
+		</div>
+	</dialog>
 
 <style>
 	dialog {
@@ -32,15 +36,19 @@
 		padding: 0;
 		margin: auto;
 	}
+
 	dialog::backdrop {
 		background: rgba(0, 0, 0, 0.3);
 	}
+
 	dialog > div {
 		padding: 1em;
 	}
+
 	dialog[open] {
 		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
 	}
+
 	@keyframes zoom {
 		from {
 			transform: scale(0.95);
@@ -49,9 +57,11 @@
 			transform: scale(1);
 		}
 	}
+
 	dialog[open]::backdrop {
 		animation: fade 0.2s ease-out;
 	}
+
 	@keyframes fade {
 		from {
 			opacity: 0;
@@ -60,6 +70,7 @@
 			opacity: 1;
 		}
 	}
+	
 	button {
 		display: block;
 	}
