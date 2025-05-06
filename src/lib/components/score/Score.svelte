0f1f277f0e099714288eb80965d22ad4927ed36e
David Hernquist <hernquistdavid@gmail.com>
2025-05-06 14:25:31 -0400

    change score imports and printouts

--- src/lib/components/score/Score.svelte
+++ src/lib/components/score/Score.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { getPlayerState } from "$lib/state/player/player.svelte";
-	import { Players } from "$lib/state/types";
+	import { Players } from "$lib/state/player/types";
+    import { getModalState } from "$lib/state/modal-state/modal-state.svelte";
 
     const { Top, Bottom } = Players;
     const playerState = getPlayerState()
     const topPlayer = playerState.player[Top];
     const bottomPlayer = playerState.player[Bottom];
+    let modalState = getModalState();
 </script>
 
-<div>{topPlayer.score} scoreboard {bottomPlayer.score}</div>
+<div>{topPlayer.score} scoreboard {bottomPlayer.score} --- {modalState.showModal}</div>
