04752630b86b150be6d684b67f801319d5b40f24
David Hernquist <hernquistdavid@gmail.com>
2025-05-06 14:04:10 -0400

    started gameOver modal screen

--- src/lib/components/game-over-modal/index.svelte
+++ src/lib/components/game-over-modal/index.svelte
@@ -2,19 +2,23 @@
     import { getGameState } from "$lib/state/game/game.svelte";
 	import { getModalState } from "$lib/state/modal-state/modal-state.svelte";
 	import { getPlayerState } from "$lib/state/player/player.svelte";
+	import { getToastState } from "$lib/state/toast/toast.svelte";
 	import { initializeGame } from "../initialize-game/utils/initializeGame";
 
     let gameState = getGameState();
     let playerState = getPlayerState();
     let modalState = getModalState();
+    let toastState = getToastState();
 
 </script>
 
 <div >
     WINNER {playerState.getWinner().name}
 
-    <button onclick={()=>{
+    <button onclick={(e)=> {
+        e.preventDefault();
         modalState.toggleModalOn();
-        initializeGame(gameState, playerState)
+        initializeGame(gameState, playerState);
+        toastState.removeAllToasts;
     }}>NEW GAME</button>
 </div>
