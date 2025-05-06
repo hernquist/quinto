a86f99b84cf76546b73c9cd5610bb2b4999442e2
David Hernquist <hernquistdavid@gmail.com>
2025-05-05 20:05:01 -0400

    start to build out game over modal

--- src/lib/components/game-over-modal/index.svelte
+++ src/lib/components/game-over-modal/index.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
-    	import { getGameState } from "$lib/state/game/game.svelte";
+    import { getGameState } from "$lib/state/game/game.svelte";
+	import { getModalState } from "$lib/state/modal-state/modal-state.svelte";
+	import { getPlayerState } from "$lib/state/player/player.svelte";
+	import { initializeGame } from "../initialize-game/utils/initializeGame";
+
+    let gameState = getGameState();
+    let playerState = getPlayerState();
+    let modalState = getModalState();
+
 </script>
 
-<div></div>
+<div >
+    WINNER {playerState.getWinner().name}
+
+    <button onclick={()=>{
+        modalState.toggleModalOn();
+        initializeGame(gameState, playerState)
+    }}>NEW GAME</button>
+</div>
