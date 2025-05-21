26fef410c9800732c852e099e59e7648b9d1c888
David Hernquist <hernquistdavid@gmail.com>
2025-05-20 21:10:59 -0400

    trying different hooks server possibility

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,26 +1,28 @@
-import type { Handle } from '@sveltejs/kit';
-import * as auth from '$lib/server/auth.js';
+export { handle } from "./auth";
 
-const handleAuth: Handle = async ({ event, resolve }) => {
-	const sessionToken = event.cookies.get(auth.sessionCookieName);
+// import type { Handle } from '@sveltejs/kit';
+// import * as auth from '$lib/server/auth.js';
 
-	if (!sessionToken) {
-		event.locals.user = null;
-		event.locals.session = null;
-		return resolve(event);
-	}
+// const handleAuth: Handle = async ({ event, resolve }) => {
+// 	const sessionToken = event.cookies.get(auth.sessionCookieName);
 
-	const { session, user } = await auth.validateSessionToken(sessionToken);
+// 	if (!sessionToken) {
+// 		event.locals.user = null;
+// 		event.locals.session = null;
+// 		return resolve(event);
+// 	}
 
-	if (session) {
-		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
-	} else {
-		auth.deleteSessionTokenCookie(event);
-	}
+// 	const { session, user } = await auth.validateSessionToken(sessionToken);
 
-	event.locals.user = user;
-	event.locals.session = session;
-	return resolve(event);
-};
+// 	if (session) {
+// 		auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+// 	} else {
+// 		auth.deleteSessionTokenCookie(event);
+// 	}
 
-export const handle: Handle = handleAuth;
+// 	event.locals.user = user;
+// 	event.locals.session = session;
+// 	return resolve(event);
+// };
+
+// export const handle: Handle = handleAuth;
