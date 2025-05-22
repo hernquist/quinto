93c031d7492277fb01a798565dcc9da1fa5ef619
David Hernquist <hernquistdavid@gmail.com>
2025-05-22 12:45:40 -0400

    updated types for adding highlights

--- src/lib/state/toast/types.ts
+++ src/lib/state/toast/types.ts
@@ -1,12 +1,34 @@
+import type { ILineItem } from "../game/types";
+
 export type IToast = {
 	id: string;
 	title: string;
 	message: string;
 };
 
+export interface IHighlightedItem extends ILineItem {
+	scoredValue: number;
+}
+
 export interface IToastState {
 	toasts: IToast[],
 	toastToTimeMap?: Record<string, number>,
+
+	highlightedSquares: IHighlightedSquare[],
+	highlightedSquaresToTimeoutMap?: Record<string, number>,
+
 	add: (title: string, message: string, durationMs?: number) => void,
 	remove: (id: string) => void,
+	
+	addHighlights: (sqaures: ILineItem[]) => void,
+	addHighlight: (square: IHighlightedItem, durationMs?: number) => void,
+	removeHighlight: (id: string) => void,
+}
+
+export interface IHighlightedSquare {
+	id: string;
+	x: number;
+	y: number;
+	value: number;
+	scoredValue: number;
 }
