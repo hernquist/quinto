02366d112934fe9029e2fe459a749ecf3b9aaa55
David Hernquist <hernquistdavid@gmail.com>
2025-05-22 12:52:02 -0400

    update toasts to include highlights

--- src/lib/state/toast/toast.svelte.ts
+++ src/lib/state/toast/toast.svelte.ts
@@ -1,9 +1,18 @@
 import { getContext, onDestroy, setContext } from 'svelte';
-import type { IToast } from './types';
+import type { IHighlightedItem, IHighlightedSquare, IToast } from './types';
+import type { ILineItem } from '../game/types';
+import { getScoredLineValue } from '../game/gameUtils';
+
+const HIGHLIGHT_DURATION = 1200;
 
 export class ToastState {
 	toasts = $state<IToast[]>([]);
 	toastToTimeoutMap = new Map<string, number>();
+
+	highlightedSquares = $state<IHighlightedSquare[]>([]);
+	highlightedSquaresToTimeoutMap = new Map();
+
+	linesIndex = 0;
 
 	constructor() {
 		onDestroy(() => {
@@ -11,6 +20,8 @@ export class ToastState {
 				clearTimeout(timeout);
 			}
 			this.toastToTimeoutMap.clear();
+
+			this.highlightedSquaresToTimeoutMap.clear();
 		});
 	}
 
@@ -30,6 +41,51 @@ export class ToastState {
 		);
 	}
 
+	private progressThroughLine(line: ILineItem[], gameMultiple: number) {
+		const scoredValue = getScoredLineValue(line, gameMultiple);
+		line.forEach((lineItem: ILineItem) => {
+			this.addHighlight({...lineItem, scoredValue} );
+		});
+		this.linesIndex++;
+	}
+
+	private resetLinesIndex(): void {
+		this.linesIndex = 0;
+	}
+
+	public addHighlights(lines: ILineItem[][], gameMultiple: number, highlightDuration = HIGHLIGHT_DURATION) {
+		this.resetLinesIndex()
+		const numberOfLines = lines.length;
+
+		const line: ILineItem[] = lines[this.linesIndex];
+		this.progressThroughLine(line, gameMultiple);
+
+		if (this.linesIndex < numberOfLines) {
+			const interval = setInterval(() => {
+				if (this.linesIndex === numberOfLines) {
+					this.removeAllHighlights();
+					clearInterval(interval);
+				} else {
+					this.removeAllHighlights();
+					const line: ILineItem[] = lines[this.linesIndex];
+					this.progressThroughLine(line, gameMultiple);
+				}	
+			}, highlightDuration);
+		}
+	}
+
+	private addHighlight(lineItem: IHighlightedItem, highlightDuration = HIGHLIGHT_DURATION) {
+		const id = crypto.randomUUID();
+
+		this.highlightedSquares.push({ ...lineItem, id })
+		this.highlightedSquaresToTimeoutMap.set(
+			id, 
+			setTimeout(() => {
+				this.removeHighlight(id);
+			}, highlightDuration)
+		)
+	}
+
 	public remove(id: string) {
 		const timeout = this.toastToTimeoutMap.get(id);
 		if (timeout) {
@@ -37,6 +93,15 @@ export class ToastState {
 			this.toastToTimeoutMap.delete(id);
 		}
 		this.toasts = this.toasts.filter((toast) => toast.id !== id);
+	}
+
+	public removeHighlight(id: string) {
+		const timeout = this.highlightedSquaresToTimeoutMap.get(id);
+		if (timeout) {
+			clearTimeout(timeout);
+			this.highlightedSquaresToTimeoutMap.delete(id);
+		}
+		this.highlightedSquares = this.highlightedSquares.filter((square) => square.id !== id);
 	}
 
 	public removeAllToasts() {
@@ -49,6 +114,19 @@ export class ToastState {
 		});
 
 		this.toasts = [];
+	}
+
+	// not sure we need this
+	public removeAllHighlights() {
+		this.highlightedSquares.forEach(({ id }) => {
+			const timeout = this.highlightedSquaresToTimeoutMap.get(id);
+			if (timeout) {
+				clearTimeout(timeout);
+				this.highlightedSquaresToTimeoutMap.delete(id);
+			}
+		});
+
+		this.highlightedSquares = [];
 	}
 }
 
