66077f93e9eda78f94a669c14464e2c5bb1d50ae
David Hernquist <hernquistdavid@gmail.com>
2025-05-27 23:25:03 -0400

    created animation component for player message

---
+++ src/lib/components/toasts/PlayerMessage.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { fade } from 'svelte/transition';
+
+    import { fly } from 'svelte/transition'
+    import { backOut } from 'svelte/easing'
+
+    let { toastState, playerPosition } = $props(); 
+</script>
+
+<div 
+    class="message__noTiles"
+    in:fly={{
+        y: 75,
+        delay: 300 * 1,
+        easing: backOut
+      }}
+>No Tiles</div>
+
+<style>
+    .message__noTiles {
+       background-color: darkgoldenrod; 
+       font-size: 20px;
+       border-radius: 4px;
+       padding: 0 4px;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+    }
+</style>
+
+
