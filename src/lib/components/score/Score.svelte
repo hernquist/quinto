a4b63366c467e3e92a250b5cddaed7113eb741c3
David Hernquist <hernquistdavid@gmail.com>
2025-05-29 10:42:59 -0400

    score with bad animation

--- src/lib/components/score/Score.svelte
+++ src/lib/components/score/Score.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { slide } from "svelte/transition";
     import { getPlayerState } from "$lib/state/player/player.svelte";
 	import { Players } from "$lib/state/player/types";
 
@@ -7,6 +8,49 @@
 
     let topPlayerScore = $derived(playerState.player[Top].score);
     let bottomPlayerScore = $derived(playerState.player[Bottom].score);
+    const duration = 300;
 </script>
 
-<div>{topPlayerScore} to {bottomPlayerScore}</div>
+<!-- TODO: spotty transition due to score layout -->
+<div class="scoreboard">
+    {#key topPlayerScore}
+        <div
+            in:slide={{ duration, delay: duration }}
+            out:slide={{ duration }}
+            class="score"
+        >
+            {topPlayerScore} 
+        </div>
+    {/key}
+        <div class="temp_text">
+            to
+        </div>
+    {#key bottomPlayerScore}
+        <div     
+            in:slide={{ duration, delay: duration }}
+            out:slide={{ duration }}
+            class="score"
+        >
+            {bottomPlayerScore} 
+        </div>
+    {/key}
+</div>
+
+<style>
+    .scoreboard {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    .score {
+        width: 20px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
+    .temp_text {
+        margin: 0 8px;
+    }
+</style>
