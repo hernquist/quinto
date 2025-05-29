33241977ca8e48e8d058ae6c962e4753ee1b2e4e
David Hernquist <hernquistdavid@gmail.com>
2025-05-29 10:50:55 -0400

    adding blur wrapper

--- src/lib/components/toasts/Toaster.svelte
+++ src/lib/components/toasts/Toaster.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
-	import { getToastState } from '$lib/state/toast/toast.svelte';
+	import { getToastState, MAIN_TOAST_DURATION } from '$lib/state/toast/toast.svelte';
 	import type { IToastState } from '$lib/state/toast/types';
   import Toast from "./Toast.svelte";
 
 	const toastState: IToastState = getToastState();
+
+  // TODO: not working
+  // function fadeBlur(node, { duration }) {
+	// 	return {
+	// 		duration,
+	// 		css: (t, u) => `
+  //       backdrop-filter: blur(${t * 15}px);
+  //       background-color: rgba(0, 0, 0, ${t * 0.4});
+  //     `
+	// 	};
+	// }
 </script>
 
-<div class="toaster">
-	{#each toastState.toasts as toast}
-		<Toast {toast} />
-	{/each}
+<div 
+  class:blurWrapper={Boolean(toastState.toasts.length)}
+>
+  <div class="toaster">
+    {#each toastState.toasts as toast}
+      <Toast {toast} />
+    {/each}
+  </div>
 </div>
 
 <style lang="postcss">
@@ -18,10 +33,21 @@
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    display: flex;
     margin-top: 1rem;
+    display: flex;
     justify-content: center;
     flex-direction: column;
     z-index: 1000;
+  }
+
+  .blurWrapper {
+    position: fixed;
+    z-index: 900;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.4);
+    backdrop-filter: blur(3px);
   }
 </style>
