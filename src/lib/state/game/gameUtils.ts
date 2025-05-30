17451d2f8d12a879fa5f5e0ca1c649c1d9df639f
David Hernquist <hernquistdavid@gmail.com>
2025-05-30 00:15:52 -0400

    add checkForContinuousTiles function to utils

--- src/lib/state/game/gameUtils.ts
+++ src/lib/state/game/gameUtils.ts
@@ -1,7 +1,8 @@
 import type { IBoard, ICoordTuple } from "$lib/components/game/types";
 import { ToastType, type IToastState } from "$lib/state/toast/types";
 import { HIGHLIGHT_DURATION } from "../toast/toast.svelte";
-import { Direction, type IDroppedTile, type ILineItem } from "./types";
+import { dependentKeyMap, independentKeyMap } from "./constants";
+import { Axis, Direction, type IDroppedTile, type IGameState, type IIsValidPlay, type ILineItem, type ITurn } from "./types";
 
 export function checkSurroundSquaresForASingleTile (board: IBoard, x: number, y: number): boolean {
     if (
@@ -47,4 +48,58 @@ export async function readLinesForScore(lines: ILineItem[][], gameMultiple: numb
             toastState.add("", `${totalLineScore}`, ToastType.TOTAL_LINE_SCORE); 
         }, lines.length * HIGHLIGHT_DURATION)
     });
+}
+
+// min max of a player's dropped tiles
+export function getMinMaxTile(droppedTiles:IDroppedTile[], direction: Direction.Horizontal | Direction.Vertical, isMin = true): IDroppedTile {
+    const key: Axis = independentKeyMap[direction];
+    const minMaxShifter = isMin ? 1 : -1;
+    return <IDroppedTile>droppedTiles.reduce((acc: IDroppedTile, curr: IDroppedTile, i: number) => 
+        i === 0 || (minMaxShifter * curr[key] < minMaxShifter * acc[key]) ? curr : acc
+    );
+}
+
+export function checkForContinuousTiles(min: IDroppedTile, max: IDroppedTile, game: IGameState): IIsValidPlay {
+    const { board, turn: { direction, droppedTiles }}: { board: IBoard, turn: ITurn } = game;
+    const key: Axis = independentKeyMap[direction];
+    const dependentKey: Axis = dependentKeyMap[direction];
+    let getMinToCheck = min[key] + 1;
+    const getMaxToCheck = max[key] - 1;
+    let noMatch = false;
+    let emptySquares = [];
+
+    while (getMaxToCheck - getMinToCheck >= 0) {
+        console.log("getMinToCheck, getMaxToCheck", getMinToCheck, getMaxToCheck);
+
+        const droppedTileMatch = droppedTiles.find((tile) => 
+            tile[key] === getMinToCheck && tile[dependentKey] === min[dependentKey]
+        );
+
+        console.log("match", droppedTileMatch)
+        if (!droppedTileMatch) {
+            let boardMatch = false;
+            board.forEach(column => {
+                column.forEach(square => {
+                    if (square[key] === getMinToCheck && square[dependentKey] === min[dependentKey] && square.tile) {
+                        boardMatch = true;
+                    }
+                })
+            })
+
+            if (!boardMatch) {
+                noMatch = true;
+                const emptySquare = {
+                    [key]: getMinToCheck,
+                    [dependentKey]: min[dependentKey],
+                }
+                emptySquares.push(emptySquare)
+            }
+        }    
+        getMinToCheck++;
+    }
+
+    return {
+        isValid: !noMatch,
+        emptySquares
+    }
 }
