26f538f71f6075b61cc2ca886ae40e7687bbcb93
David Hernquist <hernquistdavid@gmail.com>
2025-06-08 11:05:06 -0400

    add toast state to initialize game signature

--- src/lib/components/initialize-game/InitializeGame.svelte
+++ src/lib/components/initialize-game/InitializeGame.svelte
@@ -2,12 +2,14 @@
     import { getGameState } from "../../state/game/game.svelte";
     import { getPlayerState } from "$lib/state/player/player.svelte";
 	import { initializeGame } from "./utils/initializeGame";
+	import { getToastState } from "$lib/state/toast/toast.svelte";
 
     const { children } = $props();
     const gameState = getGameState();
-    const playerTileState = getPlayerState();
+    const playerState = getPlayerState();
+    const toastState = getToastState();
 
-    initializeGame(gameState, playerTileState);
+    initializeGame(gameState, playerState, toastState);
 </script>
 
 {@render children()}
