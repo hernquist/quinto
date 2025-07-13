b075fe15067bd6790d0f38ca8fb4aad91a2651a4
David Hernquist <hernquistdavid@gmail.com>
2025-07-13 15:20:24 -0400

    simple GameMultiple component

---
+++ src/lib/components/menu-bar/menu-items/game-multiple/GameMultiple.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { getGameState } from "$lib/state/game/game.svelte";
+
+    const gameState = getGameState();
+    const gameMultiple = $derived(gameState.game.gameMultiple);
+
+</script>
+
+<div class="game-multiple">
+    {gameMultiple}
+</div>
+
+<style>
+    .game-multiple {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-grow: 1;
+        font-size: 3rem;
+        line-height: 1.2;
+        padding: 0 6px 4px;
+    }
+
+</style>
