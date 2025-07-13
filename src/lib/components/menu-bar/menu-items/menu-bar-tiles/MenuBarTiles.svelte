1ec43e1fa55ea0e9347126fd44323471b3a6d45a
David Hernquist <hernquistdavid@gmail.com>
2025-07-13 15:28:47 -0400

    simple MenuBarTiles component starter

---
+++ src/lib/components/menu-bar/menu-items/menu-bar-tiles/MenuBarTiles.svelte
@@ -0,0 +1,32 @@
+<script>
+	import NumberOfTiles from "./NumberOfTiles.svelte";
+</script>
+
+<div class="tiles">
+    <div class="tiles-text">TILES</div>
+    <NumberOfTiles />
+</div>
+
+<style>
+    /* same as ViewList class */
+    .tiles {
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+        justify-content: center;
+        
+        width: 92px;
+        cursor: pointer;
+        height: calc(100% - 4px);
+
+    }
+
+    .tiles-text {
+        display: flex  ;
+        flex-grow: 1;
+
+        align-items: center;
+        justify-content: center;
+        height: 50%;
+    }
+</style>
