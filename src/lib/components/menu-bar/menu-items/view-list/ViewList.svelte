cdac40aae4ffdcb6b715f96c31f8df037f663104
David Hernquist <hernquistdavid@gmail.com>
2025-07-12 21:57:23 -0400

    building out ViewList component

---
+++ src/lib/components/menu-bar/menu-items/view-list/ViewList.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import ViewListIcon from "$lib/components/icons/ViewListIcon.svelte";
+	import { getModalState } from "$lib/state/modal-state/modal-state.svelte";
+
+    const modalState = getModalState()
+
+    const handleClick = (e: MouseEvent) => {
+        e.preventDefault();
+        modalState.toggleModalOn();
+    };
+</script>
+
+<button class="view-list" on:click={handleClick} >
+    <ViewListIcon  />
+</button>
+
+<style>
+    .view-list {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40px;
+        cursor: pointer;
+    }
+
+    .view-list:hover {
+        opacity: 0.8;
+    }
+
+    .view-list:active {
+        opacity: 0.6;
+    }
+</style>
