12eb15ce446a66a655bc9dbf8fb23c95e928f10f
David Hernquist <hernquistdavid@gmail.com>
2025-07-12 21:56:40 -0400

    building out number of tiles component

---
+++ src/lib/components/menu-bar/menu-items/tiles/NumberOfTiles.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { getGameState } from "$lib/state/game/game.svelte";
+    
+    const gameState = getGameState();
+    const numberOfTiles = $derived(gameState.game.tiles.length);
+
+</script>
+
+<div class="number-of-tiles">
+    {#if numberOfTiles > 8}
+        <span class="number-of-tiles__count">{numberOfTiles}</span>
+    {:else if numberOfTiles > 0}
+        <span class="number-of-tiles__blocks">{numberOfTiles}</span>
+    {:else}
+        <span class="number-of-tiles__count number-of-tiles__count--none">-</span>
+    {/if}
+</div>
+
+<style>
+    .number-of-tiles {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .number-of-tiles__count {
+        font-size: 1.2rem;
+    }
+
+    /* .number-of-tiles__blocks {
+    } */
+
+    .number-of-tiles__count--none {
+        font-size: 2rem;
+    }
+</style>
+
