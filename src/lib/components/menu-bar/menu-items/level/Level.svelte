25a419e9082230ef4e028248be0ff57535fc9a64
David Hernquist <hernquistdavid@gmail.com>
2025-07-13 15:26:37 -0400

    simple Level component

---
+++ src/lib/components/menu-bar/menu-items/level/Level.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { getGameState } from "$lib/state/game/game.svelte";
+	import { textLevelTuple } from "$lib/utils/textual";
+
+    const gameState = getGameState();
+    const levelIndex = $derived(gameState.game.playLevel - 1);
+</script>
+
+<div class="level">
+    LEVEL
+    {textLevelTuple[levelIndex]}
+</div>
+
+<style>
+    .level {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        width: 78px;
+    }
+</style>
