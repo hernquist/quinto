945458680ba597e3c80f84992488d5240ccf6be3
David Hernquist <hernquistdavid@gmail.com>
2025-07-19 00:05:25 -0400

    stylistic changes

---
+++ src/lib/components/player-turn-controls/PlayerTurnControls.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import FinishTurn from "../finish-turn/FinishTurn.svelte";
+	import ResetTurn from "../reset-turn/ResetTurn.svelte";
+
+    const { playerPosition, activePlayer } = $props();
+    const isActive = $derived(activePlayer === playerPosition);
+</script>
+
+<div class="playerTurnControls__container">
+    <ResetTurn {isActive} />
+    <FinishTurn {isActive} />
+</div>
+
+<style>
+    .playerTurnControls__container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        height: 45px;
+    }
+</style>
