f5a7a1953d0028cc2d965bdd6a1424d174f2703e
David Hernquist <hernquistdavid@gmail.com>
2025-07-19 00:05:01 -0400

    breaking up game play

--- src/lib/components/game-play/GamePlay.svelte
+++ src/lib/components/game-play/GamePlay.svelte
@@ -5,6 +5,7 @@
 	import { getPlayerState } from "$lib/state/player/player.svelte";
 	import { getGameState } from "$lib/state/game/game.svelte";
 	import { Players } from "$lib/state/player/types";
+	import PlayerTurnControls from "../player-turn-controls/PlayerTurnControls.svelte";
     
     const { Top, Bottom } = Players;
 
@@ -12,32 +13,41 @@
     const playerState = getPlayerState();
 </script>
 
-<div class="game-play">
+<div class="game-play__fullscreen">
     <!-- <pre>Player: {gameState.game.activePlayer}  # tiles: {gameState.game.tiles.length} Round: {gameState.game.round} Turn: {gameState.game.turn.turnStatus} Direction: {gameState.game.turn.direction} </pre> -->
     <Score />
+    <PlayerTurnControls 
+        playerPosition={gameState.game.activePlayer} 
+        activePlayer={gameState.game.activePlayer}
+    />
     <PlayerRow 
         playerPosition={Top} 
         activePlayer={gameState.game.activePlayer} 
         tiles={playerState.tiles[Top]} 
-        isComputer={playerState.player[Top].isComputer}
     />
     <Board activePlayer={gameState.game.activePlayer}/>
     <PlayerRow 
         playerPosition={Bottom} 
         activePlayer={gameState.game.activePlayer} 
         tiles={playerState.tiles[Bottom]} 
-        isComputer={playerState.player[Bottom].isComputer}
     />
+    <Score />
 </div>
-
+        
 <style>
-    .game-play {
+    .game-play__fullscreen {
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         padding: 64px 0 0 0;
         width: 100%;
-        height: 100%;
+        height: 100vh;
+    }
+
+    .game-play__container {
+        display: flex;
+        flex-direction: column;
+
     }
 </style>
