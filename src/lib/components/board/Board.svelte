7b3042a69339fe48c828a4211d42d263a74faffa
David Hernquist <hernquistdavid@gmail.com>
2025-08-12 23:41:29 -0400

    board working for basic responsiveness

--- src/lib/components/board/Board.svelte
+++ src/lib/components/board/Board.svelte
@@ -13,20 +13,45 @@
     const { activePlayer } = $props();
 </script>
 
-<div class="board__container">
-    {#each board as column, x}
-        <div class="board_row">
-            {#each column as square, y}
-                <Square {square} {x} {y} {activePlayer} {highlightedSquares} />
-            {/each}
-        </div>
-    {/each}
+<div class="outerBoard__container">
+    <div class="board__container" style:--num-of-columns={board.length}>
+        {#each board as column, x}
+            <div class="board__column" style:--num-of-rows={column.length}>
+                {#each column as square, y}
+                    <Square {square} {x} {y} {activePlayer} {highlightedSquares} />
+                {/each}
+            </div>
+        {/each}
+    </div>
 </div>
-
+    
 <style lang="postcss">
-    .board__container {
+    .outerBoard__container {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
+        align-items: center;
         justify-content: center;
+    }
+
+    .board__container {
+        display: grid;
+        justify-content: center;
+        width: calc(100% - 12px);
+        grid-template-columns: repeat(var(--num-of-columns), minmax(25px, 100px));
+        
+        @media screen and (min-width: 600px) {
+            width: 576px;
+        }
+    }
+
+    .board__column {
+        display: grid;
+        grid-template-rows: repeat(var(--num-of-rows), minmax(25px, 100px));
+        /* aspect-ratio: 1 / 1; */
+        height: calc(100vw - 24px); /* width here is key */
+
+        @media screen and (min-width: 600px) {
+            height: 576px;
+        }
     }
 </style>
