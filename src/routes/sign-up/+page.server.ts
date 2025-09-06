b70361c98de2dc080d47b8a9bfac1a0e2d94ac0b
David Hernquist <hernquistdavid@gmail.com>
2025-09-06 11:20:50 -0400

    replacing last and first name with username

--- src/routes/sign-up/+page.server.ts
+++ src/routes/sign-up/+page.server.ts
@@ -24,21 +24,22 @@ export const actions = {
     const formData = await event.request.formData();
     const email = formData.get("email") || "";
     const password = formData.get("password") || "";
-    const first_name = formData.get("first_name") || "";
-    const last_name = formData.get("last_name") || "";
+    // const first_name = formData.get("first_name") || "";
+    // const last_name = formData.get("last_name") || "";
+    const username = formData.get("username") || "";
 
     const hash = bcrypt.hashSync(password?.toString(), 10);
 
     const nUser = await db.insert(usersTable).values({
-      first_name: first_name.toString(),
-      last_name: last_name.toString(),
+    //   first_name: first_name.toString(),
+    //   last_name: last_name.toString(),
+      username: username.toString(),
       email: email.toString(),
       password: hash,
     });
 
     const token = await createAuthJWT({
-      firstName: first_name.toString(),
-      lastName: last_name.toString(),
+      username: username.toString(),
       email: email.toString(),
       id: parseInt(nUser.insertId),
     });
