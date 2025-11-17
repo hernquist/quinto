2af39f6d4e158d045259fa5f6f6fd540ff5417ec
David Hernquist <hernquistdavid@gmail.com>
2025-11-16 20:40:01 -0500

    move createNewGame out main route page server

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -16,32 +16,12 @@ export const load = async (event) => {
   const userPayload = await verifyAuthJWT(token);
 
   const user = await db.select({
-        email: usersTable.email,
-        id: usersTable.id,
-        username: usersTable.username,  
-    })
+    email: usersTable.email,
+    id: usersTable.id,
+    username: usersTable.username,  
+  })
     .from(usersTable)
     .where(eq(usersTable.id, userPayload.id));
     
   return { token, user };
 }
-
-export const actions = {
-  createNewGame: async () => {
-    console.log("HIT!!!!!!!!!!!!!!!!!!!!!!!!!")
-    await db.insert(gamesTable).values({
-      user_id: 14, // TEMPORARY USER ID
-      top_player: "H", // TEMPORARY TOP PLAYER
-      bottom_player: "C", // TEMPORARY BOTTOM PLAYER
-      winner: null,
-      top_score: 0,
-      bottom_score: 0,
-      rows: 4,
-      columns: 6,
-      multiple: 2,
-      skill_level: 3,
-    })
-  }  
-};
-
-
