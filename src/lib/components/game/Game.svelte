c8be4f00175ac89fb97433f97420fef89baa177f
David Hernquist <hernquistdavid@gmail.com>
2025-11-24 00:09:52 -0500

    added updateScore remote function

--- src/lib/components/game/Game.svelte
+++ src/lib/components/game/Game.svelte
@@ -3,6 +3,8 @@
 	import { getGameState } from "$lib/state/game/game.svelte";
 	import { getToastState, MAIN_TOAST_DURATION, HIGHLIGHT_DURATION } from "$lib/state/toast/toast.svelte";
 	import { getModalState } from "$lib/state/modal-state/modal-state.svelte";
+	import { getPlayerState } from "$lib/state/player/player.svelte";
+    import { Players } from "$lib/state/player/types";
 	import Modal from "$lib/components/modal/modal.svelte";
 	import MainModalWrapper from "$lib/components/main-modal-wrapper/MainModalWrapper.svelte";
 	import ModalHeader from "$lib/components/modal-header/ModalHeader.svelte";
@@ -10,15 +12,33 @@
     import { GameStatus } from "$lib/state/game/types";
 	import MenuBar from "../menu-bar/MenuBar.svelte";
 	import GamePlay from "../game-play/GamePlay.svelte";
+	import { makePutRequest } from "$lib/utils/api";
+	import { updateScore } from "./updateGame.remote";
     
     const gameState = getGameState();
     const modalState = getModalState();
     const toastState = getToastState();
+    const playerState = getPlayerState();
     const { numberOfLines } = $derived(toastState);
-    
+
+    const { gameId } = $props();
+    const { Top, Bottom } = Players; 
+
+    // Is this even the right way of doing it?
+    // see below
+    let fire = $state(true);
+
     // This is needs more testing and thinking. That calculation should happen out of here probably.
     $effect(() => {
-        if (gameState.game.status === GameStatus.Complete) { 
+        if (gameState.game.status === GameStatus.Complete) {
+            if (fire) {
+                updateScore({
+                    gameId, 
+                    top_score: playerState.player[Top].score,
+                    bottom_score: playerState.player[Bottom].score
+                });
+                fire = false
+            }
             setTimeout(()=> {
                 modalState.changeScreen(ModalScreen.GameOver);
                 modalState.toggleModalOn();
