3380353aa86458034cf6c37f4d6b7fffede5184f
David Hernquist <hernquistdavid@gmail.com>
2025-11-30 14:01:00 -0500

    make basic home page

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,3 +1,24 @@
+<script>
+	import { getPlayerState } from "$lib/state/player/player.svelte";
+
+    const { data } = $props();
+    const playerState = getPlayerState();
+
+    $inspect("[from home/page.svelte].data", data);
+    console.log("[from home/page.svelte].data", data);
+
+    $effect(() => {
+        if (data?.token) {
+            console.log("data", data);
+            playerState.setUser(data?.user[0]);
+        };
+    });
+</script>
+
+<style lang="postcss">
+    @reference "tailwindcss";
+</style>
+
 <div>
     HOME
 </div>
