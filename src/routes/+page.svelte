523f7f75c33c3c45ad4413f72f23b7268fa9ff8f
David Hernquist <hernquistdavid@gmail.com>
2025-11-30 14:08:04 -0500

    strip out js from page.svelte js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
-	import { getPlayerState } from "$lib/state/player/player.svelte";
+	// import { getPlayerState } from "$lib/state/player/player.svelte";
 
-    const { data } = $props();
-    const playerState = getPlayerState();
+    // const { data } = $props();
+    // const playerState = getPlayerState();
 
-    $effect(() => {
-        if (data?.token) {
-            playerState.setUser(data?.user[0]);
-        };
-    });
+    // $inspect("[from page.svelte].data", data);
+    // console.log("[from page.svelte].data", data);
 
+    // $effect(() => {
+    //     if (data?.token) {
+    //         console.log("data", data);
+    //         playerState.setUser(data?.user[0]);
+    //     };
+    // });
 </script>
 
 <style lang="postcss">
