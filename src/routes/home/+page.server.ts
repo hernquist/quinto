edeb6ac4af2f568524852fc084c5a38fc5f35322
David Hernquist <hernquistdavid@gmail.com>
2025-11-30 18:24:15 -0500

    home server file

--- src/routes/home/+page.server.ts
+++ src/routes/home/+page.server.ts
@@ -3,10 +3,35 @@ import { game as gamesTable } from "$lib/server/db/schema";
 import { verifyAuthJWT } from "$lib/server/jwt.js";
 import { boards, Sizes } from "$lib/constants/boards.js";
 import { redirect } from '@sveltejs/kit';
+import { user as usersTable } from "$lib/server/db/schema";
+import { eq } from "drizzle-orm";
+
+export const load = async (event) => {
+  // get the token from the cookie
+  const token = event.cookies.get("auth_token");
+
+  // return null token and null user
+  if (!token) {
+    return { token: null, user: null };
+  }
+  
+  const userPayload = await verifyAuthJWT(token);
+
+  const user = await db.select({
+    email: usersTable.email,
+    id: usersTable.id,
+    username: usersTable.username,  
+  })
+    .from(usersTable)
+    .where(eq(usersTable.id, userPayload.id));
+    
+  // if there is a token, set user in store on the client
+  return { token, user };
+}
 
 export const actions = {
     // TODO: this is a duplicated action
-    // // TODO: is this a candidate for a remote function?
+    // TODO: is this a candidate for a remote function?
     createNewGame: async ({ request, cookies, params }) => {
         const data = await request.formData();
         let multiple, level, rows, columns;
