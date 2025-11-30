8e9c465f55eddeae4d233bbac0f41cd71fbd613d
David Hernquist <hernquistdavid@gmail.com>
2025-11-30 14:08:20 -0500

    clean up strip out

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,4 @@
 <script>
-	// import { getPlayerState } from "$lib/state/player/player.svelte";
-
-    // const { data } = $props();
-    // const playerState = getPlayerState();
-
-    // $inspect("[from page.svelte].data", data);
-    // console.log("[from page.svelte].data", data);
-
-    // $effect(() => {
-    //     if (data?.token) {
-    //         console.log("data", data);
-    //         playerState.setUser(data?.user[0]);
-    //     };
-    // });
 </script>
 
 <style lang="postcss">
