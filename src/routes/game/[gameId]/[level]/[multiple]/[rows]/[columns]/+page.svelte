0ec05302a25fd38f06f33e7f222919fd6f63381f
David Hernquist <hernquistdavid@gmail.com>
2025-11-30 20:52:34 -0500

    updating state

--- src/routes/game/[gameId]/[level]/[multiple]/[rows]/[columns]/+page.svelte
+++ src/routes/game/[gameId]/[level]/[multiple]/[rows]/[columns]/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { onMount } from "svelte";
 	import Game from "$lib/components/game/Game.svelte";
 	import { initializeGame } from "$lib/components/initialize-game/utils/initializeGame";
 	import { getGameState } from "$lib/state/game/game.svelte";
 	import { getPlayerState } from "$lib/state/player/player.svelte";
 	import { getToastState } from "$lib/state/toast/toast.svelte";
-	import { onMount } from "svelte";
+    import { setUser } from "$lib/utils/setUser";
+
 	import type { PageProps } from '../../../../$types';
 
     const gameState = getGameState();
@@ -25,10 +27,21 @@
         initializeGame(gameState, playerState, toastState, options);
     }
 
-    onMount(() => setGame());;
+    onMount(() => setGame());
+
+    function handleClick() {
+        console.log("click playerState", 
+            JSON.parse(JSON.stringify(playerState))
+        );
+    }   
+    
+    $effect(() => {
+        setUser(playerState, data);
+    });
 </script>
 
 <Game gameId={gameId} />
+<button style="margin-top: 80px; height: 100px; position: relative" onclick={handleClick}>BUTTON</button>
 
 <style lang="postcss">
     @reference "tailwindcss";
