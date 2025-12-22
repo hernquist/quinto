cc5f0115a9b36a4727443451f0fca9a3fab24493
David Hernquist <hernquistdavid@gmail.com>
2025-12-21 20:56:38 -0500

    adding user page

--- src/routes/user/[userId]/+page.svelte
+++ src/routes/user/[userId]/+page.svelte
@@ -1,4 +1,42 @@
-<div>
-    USER PAGE 
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { getPlayerState } from "$lib/state/player/player.svelte.js";
+
+    const playerState = getPlayerState();
+    
+    function handleRedirect() {        
+        // clear player state -- a bit hacky but the client-side clearUser is getting 
+        playerState.clearUser();
+        goto('/sign-out');
+    }
+</script>
+
+<div class="user__page">
+    <h1>USER PAGE</h1>
+
+    <div>
+        <div>USERNAME: {playerState.humanPlayer.user?.username}</div>
+        <button on:click={handleRedirect}>SIGN OUT</button>
+    </div>
 </div>
 
+<style lang="postcss">
+    .user__page {
+        height: 100vh;
+        background-color: #E0D268;
+        padding: 0 24px;
+    }
+
+    h1 {
+        padding: 100px 0 0 0;
+    }
+
+    button {
+        background-color: #447799;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 5px;
+        border: none;
+        cursor: pointer;
+    }
+</style>
