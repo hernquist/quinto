26c371db20bc32308de49c60e4968f0cab57d4d0
David Hernquist <hernquistdavid@gmail.com>
2025-12-21 21:44:48 -0500

    switching out page server createNewGame

--- src/routes/game/[gameId]/[level]/[multiple]/[rows]/[columns]/+page.server.ts
+++ src/routes/game/[gameId]/[level]/[multiple]/[rows]/[columns]/+page.server.ts
@@ -1,11 +1,11 @@
 import { db } from "$lib/server/db";
 import { game as gamesTable } from "$lib/server/db/schema";
 import { verifyAuthJWT } from "$lib/server/jwt.js";
-import { boards, Sizes } from "$lib/constants/boards.js";
-import { error, fail, redirect, type RequestHandler } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from "../../../$types";
 import { eq } from "drizzle-orm";
 import { user as usersTable } from "$lib/server/db/schema";
+import { createNewGame } from "$lib/utils/createNewGame";
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
     // get the token from the cookie
@@ -56,49 +56,6 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
 };
 
 export const actions = {
-    // TODO: this is a duplicated action
-    // TODO: is this a candidate for a remote function?
-    createNewGame: async ({ request, cookies, params }) => {
-        const data = await request.formData();
-        let multiple, level, rows, columns;
-
-        if (data instanceof FormData && data.has('multiple') || data.has('boardType') || data.has('skillLevel')) {
-            multiple = data.get('multiple');
-            level = data.get('skillLevel');
-            const boardType = data.get('boardType') as Sizes;
-            rows = boards[boardType].rows;
-            columns = boards[boardType].columns; 
-        } else {
-            multiple = params.multiple;
-            level = params.level;
-            rows = params.rows;
-            columns = params.columns;
-        }
-
-        const token = cookies.get("auth_token");
-
-        // if there is a token, set user in store
-        if (!token) {
-            console.error("NO TOKEN IN game page server ts");
-            return {}
-        }
-          
-        const userPayload = await verifyAuthJWT(token);
-        const user_id = userPayload.id
-
-        const [game] = await db.insert(gamesTable).values({
-            user_id,
-            top_player: "H", // TEMPORARY TOP PLAYER
-            bottom_player: "C", // TEMPORARY BOTTOM PLAYER
-            winner: null,
-            top_score: 0,
-            bottom_score: 0,
-            rows,
-            columns,
-            multiple,
-            skill_level: level
-        }).returning();
-
-        redirect(303, `/game/${game.id}/${level}/${multiple}/${rows}/${columns}`);
-    },
+    // @ts-ignore-next-line
+    createNewGame
 };
