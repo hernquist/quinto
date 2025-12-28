f92caa2f7daea027246e0c73062e84a296ea62c5
David Hernquist <hernquistdavid@gmail.com>
2025-12-27 22:33:18 -0500

    added highscore setter to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
 	import MenuBar from "$lib/components/menu-bar/MenuBar.svelte";
-
 	import Modal from "$lib/components/modal/modal.svelte";
 	import MainModalWrapper from "$lib/components/main-modal-wrapper/MainModalWrapper.svelte";
 	import ModalHeader from "$lib/components/modal-header/ModalHeader.svelte";
-	import { ModalScreen } from "$lib/state/modal-state/types";
-    import { GameStatus } from "$lib/state/game/types";
-
 	import { setPlayerState } from '$lib/state/player/player.svelte';
 	import { setGameState } from '$lib/state/game/game.svelte';
 	import { setToastState } from '$lib/state/toast/toast.svelte';
+	import { setHighscoresState } from "$lib/state/highscores/highscores.svelte";
 	import { setModalState, getModalState } from '$lib/state/modal-state/modal-state.svelte';
 	import Toaster from '$lib/components/toasts/Toaster.svelte';
 	import '../app.css';
 
 	let { children } = $props();
+
 	setPlayerState();
 	setGameState();
 	setToastState();
 	setModalState();
+	setHighscoresState()
 
     const modalState = getModalState();
 </script>
@@ -26,6 +25,7 @@
 <Toaster />
 
 <MenuBar />
+
 {#if modalState.showModal === true}
 	<Modal>
 		{#snippet header()}
