c081e66d87092816fedac3eee4b00e7954c7c345
David Hernquist <hernquistdavid@gmail.com>
2025-12-27 22:20:46 -0500

    building simple UI for HighscoresModal

---
+++ src/lib/components/highscores-modal/HighscoresModal.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { getHighscoresState } from "$lib/state/highscores/highscores.svelte.js";
+
+    const { highscores = [] } = getHighscoresState();
+    console.log("highscores", highscores);
+</script>
+
+<div>
+    <ul>
+        {#each highscores.scores as highscore}
+            <li>{highscore.rows} by {highscore.columns}</li>
+        {/each}
+    </ul>
+</div>
