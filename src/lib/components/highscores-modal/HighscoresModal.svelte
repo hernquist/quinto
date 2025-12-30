dbf01cc69bbb04e0a652c69def32e3f441bb32c9
David Hernquist <hernquistdavid@gmail.com>
2025-12-29 23:39:21 -0500

    add basic UI render on the modal

--- src/lib/components/highscores-modal/HighscoresModal.svelte
+++ src/lib/components/highscores-modal/HighscoresModal.svelte
@@ -2,13 +2,12 @@
     import { getHighscoresState } from "$lib/state/highscores/highscores.svelte.js";
 
     const { highscores = [] } = getHighscoresState();
-    console.log("highscores", highscores);
 </script>
 
 <div>
     <ul>
         {#each highscores.scores as highscore}
-            <li>{highscore.rows} by {highscore.columns}</li>
+            <li>{highscore.rows} by {highscore.columns} -- {highscore.username} -- {highscore.topScore} </li>
         {/each}
     </ul>
 </div>
