cc3beef07fc63ddaad2be165a99089ee26c19bc2
David Hernquist <hernquistdavid@gmail.com>
2026-02-28 18:57:05 -0500

    added directions route

---
+++ src/routes/directions/+page.svelte
@@ -0,0 +1,22 @@
+<div class="directions__fullscreen">
+  <h1>Directions</h1>
+</div>
+
+<style lang="postcss">
+  .directions__fullscreen {
+    position: fixed;
+    top: 64px;
+    width: 100%;
+    min-height: 100vh;
+    background: #447799;
+    padding: 24px;
+  }
+
+  .directions__fullscreen h1 {
+    margin: 0;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-weight: 700;
+    font-size: 28px;
+    color: #fff;
+  }
+</style>
